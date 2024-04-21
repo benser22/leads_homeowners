c6fd71b8afc17498b3a025a7c76d2acebb80caf4
Benjamín Serrano Friedlander <benser22@hotmail.com>
2024-04-21 19:37:26 -0300

    feat(section): lastsection was created. Todo: footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import Navbar from "./components/Navbar/Navbar";
-import Layout from "./components/ui/Layout";
 import Hero from "./components/Hero/Hero";
 import OneSection from "./components/Sections/OneSection";
 import AboutSection from "./components/Sections/AboutSection";
 import OurProjects from "./components/Sections/OurProjects";
 import Testimonials from "./components/Sections/Testimonials";
+import LastSection from "./components/Sections/LastSection";
 
 function App() {
   return (
@@ -15,6 +15,7 @@ function App() {
       <AboutSection />
       <OurProjects />
       <Testimonials />
+      <LastSection />
     </main>
   );
 }
--- src/components/Sections/Testimonials.jsx
+++ src/components/Sections/Testimonials.jsx
@@ -9,7 +9,7 @@ const Testimonials = () => {
   const testimonials = data["section-six"].testimonials;
   const title = data["section-six"].title;
   const settings = {
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 5,
@@ -45,27 +45,31 @@ const Testimonials = () => {
   };
 
   return (
-    <div className="px-4 py-16">
-      <section id="testimonials">
-        <Layout>
-          <div className="flex flex-col text-center w-full">
+    <Layout>
+      <div
+        className="px-4 py-16"
+        style={{
+          backgroundImage: `url(${data["section-two"].bg})`,
+          backgroundSize: "cover",
+          width: "100%",
+        }}
+      >
+        <section id="testimonials">
+          <div className="flex flex-col text-center w-[98%]">
             <div className="flex flex-col w-full text-xl lg:text-2xl 2xl:text-3xl font-bold mb-14">
               <p className="text-light-blue-custom mx-auto">{title}</p>
               <hr className="bg-light-blue-custom mt-6 h-2 lg:h-3 w-28 mx-auto rounded-xl mb-4"></hr>
             </div>
-            <Slider {...settings} className="h-[18rem] px-6 w-[95vw] mx-auto">
+            <Slider {...settings} className="h-[22rem] px-6 w-[95%] mx-auto">
               {testimonials.map((testimonial) => (
-                <div key={testimonial.id} className="space-y-2 h-[18rem]">
+                <div key={testimonial.id} className="space-y-2 h-[22rem]">
                   <img
                     src={testimonial.photo}
                     alt={testimonial.name}
                     className="h-16 mx-auto rounded-full -mb-8"
-                    style={{
-                      border: "6px solid rgba(33, 123, 244, 0.2)",
-                    }}
                   />
                   <div
-                    className="flex flex-col text-left px-4 py-6 rounded-lg mx-2"
+                    className="flex flex-col text-left px-4 py-6 rounded-lg mx-2 bg-white"
                     style={{ boxShadow: "0px 0px 2px 1px rgba(0, 0, 0, 0.08)" }}
                   >
                     <div className="flex gap-1">
@@ -94,9 +98,9 @@ const Testimonials = () => {
               ))}
             </Slider>
           </div>
-        </Layout>
-      </section>
-    </div>
+        </section>
+      </div>
+    </Layout>
   );
 };
 
